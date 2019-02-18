a5ae9f6b1ceb7905355525673da6fa4a818e14a5
Marcelo Freitas de Carvalho <marcelo.carvalho@ilumno.com>
2019-02-18 15:02:10 -0300

    teste inicial

---
+++ WebApplication1/WebApplication2/ClienteDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2
+{
+    public class ClienteDb : DbContext
+    {
+        public ClienteDb(DbContextOptions<ClienteDb> options)
+            : base(options) { }
+        public ClienteDb() { }
+        public DbSet<Cliente> Cliente { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseMySql(@"Server = sql10.freemysqlhosting.net; DataBase = sql10185723; Uid = sql10185723; Pwd = maVj1MHQaz;");
+
+    }
+}
---
+++ WebApplication1/WebApplication2/Controllers/ClienteController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClienteController : Controller
+    {
+        // GET: api/cliente
+        [HttpGet]
+        public IEnumerable<Cliente> Get()
+        {
+
+            using (ClienteDb db = new ClienteDb())
+            {
+                return db.Cliente.ToList();
+            }
+            
+        }
+
+        // GET api/cliente/5
+        [HttpGet("{id}")]
+        public Cliente Get(int id)
+        {
+            using (ClienteDb db = new ClienteDb())
+            {
+                return db.Cliente.First(t => t.Id == id);
+            }
+        }
+
+        // POST api/cliente
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/cliente/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/cliente/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
